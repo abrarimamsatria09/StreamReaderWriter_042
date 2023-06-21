600cd081336821177fbc570e986bee0d686ecc52
abrarimamsatria09 <092004abrar@gmail.com>
2023-06-21 14:55:40 +0700

    menulis dan memasukkan nikai dari 'baris' ke dalam file

--- ioFile_042/ioFile_042.cpp
+++ ioFile_042/ioFile_042.cpp
@@ -20,6 +20,8 @@ int main() {
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukkan karakter q
 		if (baris == "q") break;
+		//menulis dan memasukkan nikai dari 'baris' ke dalam file
+		outfile << baris << endl;
 	}
 }
 
